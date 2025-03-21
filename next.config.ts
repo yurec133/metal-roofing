ab48116e2f1ec36aaa7a1bbecd6da6b7e70c7cbe
Yurec <yurec133@gmail.com~>
2025-03-21 05:54:30 +0200

    fix config img config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
     reactStrictMode: true,
     output: "export",
+    basePath: isProd ? "/metal-roofing" : "",
+    assetPrefix: isProd ? "/metal-roofing/" : "",
     images: {
         unoptimized: true,
     },
